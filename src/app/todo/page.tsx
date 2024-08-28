49109b751ca0b97870fda82250615d19d2576145
Adelie <lsh77392125@gmail.com>
2024-08-28 17:06:18 +0900

    20240828

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -25,16 +25,21 @@ export default function Todo() {
   const onComplated = "visible w-32 h-9 bg-red-custom-2 text-white border-4 border-white mx-1";
   const offComplated = "visible w-32 h-9 bg-red-custom-3 text-black mx-1";
 
+  const notChecked = "visible bg-orange-custom-1 hover:bg-orange-custom-2";
+  const Checked = "visible bg-green-custom-1";
+
   const [activeButton, setActiveButton] = useState<'all' | 'active' | 'completed'>('all')
+  const [checkState, setCheckState] = useState<"all" | "ACTIVE" | "COMPLETED">("all")
   const [isCheckboxChecked, setIsCheckboxChecked] = useState(false)
   const [content, setContent] = useState("")
   const [addList, setAddList] = useState(false)
   const [monthFilter, setMonthFillter] = useState<number>(month)
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  const [checkedIndex, setCheckedIndex] = useState<number | null>(null);
   const [dbContent, setDBContent] = useState<string[]>([])
   const [dbIndex, setDBIndex] = useState<number[]>([])
   const [dbData, setDBData] = useState<string[]>([])
-  var sortingState : string = "desc";
+  const [sortingState, setSortingState] = useState< "desc"| "asc">("desc");
   const [updateLists, setUpdateLists] = useState(false)
   const [toggleBtn, setToggleBtn] = useState(false)
   const [descState, setDescState] = useState(false)
@@ -54,11 +59,11 @@ export default function Todo() {
 
   const useToggleBtn = () => {
     if(toggleBtn) {
-      sortingState = "desc";
+      setSortingState('desc');
       setToggleBtn(false);
       setDescState(false);
     } else {
-      sortingState = "asc";
+      setSortingState('asc')
       setToggleBtn(true);
       setDescState(true);
     }
@@ -66,21 +71,6 @@ export default function Todo() {
     Filter();
   }
 
-  useEffect(() => {
-    const loadData = async () => {
-      try {
-        const response = await fetch('http://localhost:8080/time', { method: 'GET' });
-        const data = await response.json();
-        
-        const contentArray = data.map((item: { content: string }) => item.content);
-        
-        setDBContent(contentArray);
-      } catch (error) {
-        console.error('Failed to load data', error);
-      }
-    }
-    loadData();
-  }, []);
 
   async function loadData() {
     try {
@@ -100,24 +90,26 @@ export default function Todo() {
     setContent(dbData[index])
   };
 
+  const handleCheck = (index: number) => {
+    setCheckedIndex(index);
+  };
+
   const List = () => {
     if(dbIndex.length === 0) {
       return
     }
-
     dbData.length = 0;
-
     for(let i=0; i<dbIndex.length; i++) { 
       dbData[i] = dbContent[(dbIndex[i]-1)];
     }
-    
     return (
       <div id="List">
       {dbData.map((item, indexList) => (
         <div key={indexList} className="relative">
           <button
-            className={`w-110 h-14 rounded-lg mt-4 ${isCheckboxChecked ? `bg-green-custom-1` : `bg-orange-custom-1 hover:bg-orange-custom-2`}`}
-            onClick={() => {handleEdit(indexList)}}
+            className={`w-110 h-14 rounded-lg mt-4  ${isCheckboxChecked ? Checked : notChecked}`} //체크를 전부 따로따로 동작되게 해야함, 체크 박스를 누르면 수정 UI가 뜨지 않아야함
+            onClick={() => {handleEdit(indexList)
+              handleCheck(indexList)}}
           >
             <label className="relative inline-flex cursor-pointer mr-96 ml-5">
               <input
@@ -128,6 +120,11 @@ export default function Todo() {
               <span className="text-xl ml-2 w-96 text-left">{item}</span>
             </label>
           </button>
+          {checkedIndex === indexList && ( //각 리스트의 인덱스로 따로 따로 HTML을 실행해 여기서 색을 바꿀 생각 -> 하지만 CSS(백그라운드 색) 이 덮어 씌워지지 않음
+            <div>
+              
+            </div>
+          )}
           {activeIndex === indexList && (
             <div className="absolute w-120 -mt-12 ml-10 flex z-10 rounded-lg">
               <input
@@ -179,9 +176,7 @@ export default function Todo() {
         body: JSON.stringify({time : today, month : month, content : content}),
       })
       const data = await response.json();
-      
       const contentArray = data.map((item: { content: string }) => item.content);
-      
       setDBContent(contentArray);
       loadData();
     } catch(err) {
@@ -193,7 +188,6 @@ export default function Todo() {
     if(descState === true) {
       Updateindex = (dbContent.length - (Updateindex+1))
     }
-
     try {
       const response = await fetch('http://localhost:8080/time/update', {method : 'POST',
         headers: {"Content-Type": "application/json",},
@@ -219,25 +213,45 @@ export default function Todo() {
 
   async function Filter() {
     try {
-      const response = await fetch('http://localhost:8080/time/filter', {method : 'POST',
-        headers: {"Content-Type": "application/json",},
-        body: JSON.stringify({month : fetchMonthFilter}),
-      })
-      const data = await response.json();
-      if(sortingState === "asc") { //토글 스위치하고 변수 상태가 맞지 않음, 정확히는 지금 상태의 sortingState를 비교하지 않고 전 상태를 비교함
-        data.reverse(); //const를 쓰면 한번 밀리고, let이나 var를 쓰면 할때마다 다시 초기화가 선언되서 월로 넘어갈 때 CSS는 되지만, 변수가 유지가 안됨 -> 포기 ㅈㅈ
-      }
-      if(data.length === 0) {
-        setDBIndex([]);
-      }
-      const indexArray = data.map((item: { id: number }) => item.id);
-      setDBIndex(indexArray);
-      loadData();
+        const response = await fetch('http://localhost:8080/time/filter', {method : 'POST',
+          headers: {"Content-Type": "application/json",},
+          body: JSON.stringify({month : fetchMonthFilter, checkState : checkState}), //checkState이 string아니여가지고 타입 안맞아서 백엔드에 넘어갈 때 오류 나올듯 1.activeButton을 써서 백엔드를 불리언으로 갖고오기, 2.checkState를 String으로 바꾸기 1번 한번 해보자
+        })
+        const data = await response.json();
+        if(sortingState === "asc") {
+          data.reverse(); 
+        }
+        if(data.length === 0) {
+          setDBIndex([]);
+        }
+        const indexArray = data.map((item: { id: number }) => item.id);
+        setDBIndex(indexArray);
+        loadData();
     } catch(err) {
       console.error(err)
     }
   }
 
+  useEffect(()=>{
+    Filter()
+  },[sortingState]);
+
+  
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/time', { method: 'GET' });
+        const data = await response.json();
+        
+        const contentArray = data.map((item: { content: string }) => item.content);
+        
+        setDBContent(contentArray);
+      } catch (error) {
+        console.error('Failed to load data', error);
+      }
+    }
+    loadData();
+  }, []);
 
   return(
     <div className="bg-orange-custom-1 flex justify-center min-h-screen items-center">
